332817701612b14285a63bae279af2d23689c486
Jiang Sheng <gisonrg@gmail.com>
2014-02-03 01:06:17 +0800

    homepage

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+//header contains <body> tag
+require("view/common/header.php");
+//footer contains </body> and </html> tag
+require("view/common/footer.php");
+display_header("Feedback");
+
+
+require("view/feedbackform.inc");
+
+display_footer();
+?>
+
+
+
+
+
