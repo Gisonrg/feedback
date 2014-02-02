a0be6e7753382d1be1d402dad9d4a59a91ce81a7
Jiang Sheng <gisonrg@gmail.com>
2014-02-03 01:04:55 +0800

    config

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+//database related
+define('DB_USERNAME', 'clubadmin');
+define('DB_PASSWORD', '12345');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'comclub');
+
+//File path
+define('CSS_PATH','static/css/');
+define('COMMON_PATH','view/common/');
+
+?>
